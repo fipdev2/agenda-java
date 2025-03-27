fd639452703095a7cf4e94d274b1fda596785e44
= <=>
2025-03-27 03:53:16 -0300

    feat: integrate Panel class into main application flow

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,12 @@ import agenda.Agenda;
 
 import javax.swing.*;
 
+import panel.Panel;
+
 public class Main {
     public static void main(String[] args) {
+
         Agenda agenda = new Agenda();
-        JTextArea input = new JTextArea();
-        String text = input.getText();
-        agenda.write(text);
+        SwingUtilities.invokeLater(() -> new Panel(agenda));
     }
 }
